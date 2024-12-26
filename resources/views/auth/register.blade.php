329da0d4245aeffdc03c5470f5683a8a07a1cdc6
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-12-26 20:22:09 +0200

    Make some changes in the env file, connecting my system with database

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -53,24 +53,10 @@
 
 
 <!DOCTYPE html>
-<!--
-Author: Keenthemes
-Product Name: MetronicProduct Version: 8.2.9
-Purchase: https://1.envato.market/Vm7VRE
-Website: http://www.keenthemes.com
-Contact: support@keenthemes.com
-Follow: www.twitter.com/keenthemes
-Dribbble: www.dribbble.com/keenthemes
-Like: www.facebook.com/keenthemes
-License: For each use you must have a valid license purchased only from above link in order to legally use the theme for your project.
--->
 <html lang="en" >
-    <!--begin::Head-->
-    
-<!-- Mirrored from preview.keenthemes.com/metronic8/demo1/authentication/layouts/overlay/sign-up.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 25 Dec 2024 19:52:49 GMT -->
-<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
 <head>
-        <title>Metronic - The World's #1 Selling Tailwind CSS & Bootstrap Admin Template by KeenThemes</title>
+         <title>ByteBrain - File Manager System</title>
         <meta charset="utf-8"/>
         <meta name="description" content="
             The most advanced Tailwind CSS & Bootstrap 5 Admin Theme with 40 unique prebuilt layouts on Themeforest trusted by 100,000 beginners and professionals. Multi-demo,
