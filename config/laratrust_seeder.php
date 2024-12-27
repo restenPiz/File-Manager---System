f8f0a3effa6736f13557b8fbc76f482686c31a6b
restenPiz <99766054+restenPiz@users.noreply.github.com>
2024-12-27 11:07:20 +0200

    Doing some changes in the laratrust Seeder

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -7,9 +7,12 @@ return [
 
     'roles_structure' => [
         'superadministrator' => [
-            'users' => 'c,r,u,d',
-            'payments' => 'c,r,u,d',
-            'profile' => 'r,u',
+            'users' => 'c,r,u,d,a,s,p',
+            'files' => 'c,r,u,d,a,s,p',
+            'folders' => 'c,r,u,d,a,s,p',
+            'roles' => 'c,r,u,d,a,s,p',
+            'permissions' => 'c,r,u,d,a,s,p',
+            'settings' => 'c,r,u,d,',
         ],
     ],
 
@@ -19,5 +22,7 @@ return [
         'u' => 'update',
         'd' => 'delete',
         's' => 'show',
+        'a' => 'upload',
+        'p' => 'share'
     ],
 ];
