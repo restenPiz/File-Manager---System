a06d3cc5f39805322ce9d481dff42f21798b4cda
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-06 00:27:01 +0200

    Some updates in the application

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+//*Start with the main route
 Route::get('/', function () {
     return Inertia::render('Auth/Login', [
         'canLogin' => Route::has('login'),
