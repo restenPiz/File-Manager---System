419660694558c23de8ad17f9d1a4bf1b9aa0af55
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-06 13:08:35 +0200

    Update my database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,5 +11,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(LaratrustSeeder::class);
+
+        //*Inicio da seeder responsavel pelo registro de dados do utilizador
+        $user = User::create([
+            'name' => 'Super Admin',
+            'email' => 'super@gmail.com',
+            'password' => Hash::make('admin@123'),
+        ]);
+
+        $user->addRole('super');
     }
 }
