30be9555da5e0c693ca9644733283838d10e125e
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-06 13:47:44 +0200

    Some updates in the application

--- app/Http/Middleware/CheckUserRole.php
+++ app/Http/Middleware/CheckUserRole.php
@@ -12,7 +12,7 @@ class CheckUserRole
     public function handle(Request $request, Closure $next): Response
     {
         //* Verifique se o usuário está autenticado e tem a role 'super'
-        if (Auth::check() && Auth::user()->hasRole !== 'super') {
+        if (Auth::check() && !Auth::user()->hasRole('super')) {
             return redirect()->route('login');
         }
 
