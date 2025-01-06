ae14c26fb9bbd0a7f22e28ecf04e71ea8f5e97ac
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-06 13:15:46 +0200

    Solving some bugs that the system gives to me

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laratrust\Contracts\LaratrustUser;
+use Laratrust\Traits\HasRolesAndPermissions;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements LaratrustUser
