2a7eb8127382e4d19b57259951b1568925a232fc
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-07 20:35:59 +0200

    Some updates in the application

--- app/Http/Controllers/folderController.php
+++ app/Http/Controllers/folderController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class folderController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        Folder::create($request->validate([
+            'Folder_name' => ['required', 'max:50'],
+            'id_user' => ['required', 'max:50'],
+            'Parent_id',
+        ]));
+
+        return to_route('folder');
+    }
 }
