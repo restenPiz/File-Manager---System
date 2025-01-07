5174857d846b2930af82c38834efeeffb3149480
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-07 20:19:51 +0200

    Working on my migration

--- database/migrations/2025_01_07_174758_create_folders_table.php
+++ database/migrations/2025_01_07_174758_create_folders_table.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->id();
+            $table->string('Folder_name');
+            $table->string('Parent_id');
+
+            //*Start with the foreign key
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('folders');
--- database/migrations/2025_01_07_174839_create_files_table.php
+++ database/migrations/2025_01_07_174839_create_files_table.php
@@ -4,22 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->string('File_name');
+            $table->string('Path');
+            $table->string('Quantity');
+
+            //*Start with the foreign key
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('id_folder');
+            $table->foreign('id_folder')->references('id')->on('folders')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('files');
