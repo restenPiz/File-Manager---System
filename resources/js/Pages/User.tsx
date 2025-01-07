479c60d02076a9bafd410065735a8b17a9c1fc9e
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-07 13:03:39 +0200

    Add a title and button on component manage user

--- resources/js/Pages/User.tsx
+++ resources/js/Pages/User.tsx
@@ -2,6 +2,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PageProps } from "@/types";
 import { Head } from "@inertiajs/react";
 import TableUser from "@/Components/TableUser";
+import { Button } from "flowbite-react";
 
 export default function User({ auth }: PageProps) {
     return (
@@ -15,10 +16,18 @@ export default function User({ auth }: PageProps) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="flex justify-between items-center mb-8">
+                        <h2 className="text-xl font-semibold text-gray-800 dark:text-gray-200">
+                            All Users
+                        </h2>
+                        <Button className="bg-blue-950">
+                            Add a new user
+                        </Button>
+                    </div>
+
                     <TableUser></TableUser>
                 </div>
             </div>
-
 
         </AuthenticatedLayout>
     );
