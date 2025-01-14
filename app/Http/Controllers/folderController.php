844f64c97f85c17faf65c52333eb08403d502528
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-14 10:33:06 +0200

    Some updates in the application

--- app/Http/Controllers/folderController.php
+++ app/Http/Controllers/folderController.php
@@ -18,13 +18,31 @@ class folderController extends Controller
     }
     public function store(Request $request)
     {
-        // dd($request->all());
         $table = new Folder();
 
         $table->Folder_name = $request->input('Folder_name');
         $table->id_user = $request->input('id_user');
         $table->Parent_id = $request->input('Parent_id');
         $table->save();
+
+        return to_route('folder');
+    }
+    public function update(Request $request, $id)
+    {
+        $table = Folder::findOrFail($id);
+
+        $table->Folder_name = $request->input('Folder_name');
+        $table->id_user = $request->input('id_user');
+        $table->Parent_id = $request->input('Parent_id');
+        $table->save();
+
+        return to_route('folder');
+    }
+    public function delete($id)
+    {
+        $folder = Folder::findOrFail($id);
+
+        $folder->delete();
 
         return to_route('folder');
     }
