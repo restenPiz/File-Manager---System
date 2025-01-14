c0aa43783a3edb0ab48f2cfb71b06a9918bfe188
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-14 16:06:16 +0200

    Some updates in the application - make some updates in the folder section

--- resources/js/Pages/Folder.tsx
+++ resources/js/Pages/Folder.tsx
@@ -150,14 +150,6 @@ export default function Folder({ auth, folders }: PageProps) {
                                         </Dropdown.Item>
                                         <Dropdown.Item>
                                             <a
-                                                href="#"
-                                                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                                            >
-                                                Export Data
-                                            </a>
-                                        </Dropdown.Item>
-                                        <Dropdown.Item>
-                                            <a
                                                 onClick={() => openDeleteModal(folder.id)}
                                                 className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                                             >
