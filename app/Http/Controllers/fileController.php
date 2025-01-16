7ce4894c5e26381db0249b960cdcb9c94879f450
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-16 14:01:32 +0200

    Some updates in the application

--- app/Http/Controllers/fileController.php
+++ app/Http/Controllers/fileController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\File;
 use Illuminate\Http\Request;
 
 class fileController extends Controller
 {
-    //
+    public function index()
+    {
+        return to_route('File');
+    }
+    public function store(Request $request)
+    {
+        $file = new File();
+
+        $file->File_name = $request->input('File_name');
+        $file->Path = $request->input('Path');
+        $file->Quantity = $request->input('Quantity');
+        $file->id_folder = $request->input('id_folder');
+
+        $file->save();
+
+        return to_route('file');
+    }
 }
