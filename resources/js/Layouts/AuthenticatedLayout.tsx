c7652a20fb2f0db08f2181685c2f1fe8a26edf85
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-17 14:06:18 +0200

    Working with color, changing the color of menu and navbar

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -158,7 +158,7 @@ export default function Authenticated({ user, header, children }: PropsWithChild
             </nav>
 
             {header && (
-                <header className="bg-white shadow">
+                <header className="bg-purple-100 shadow">
                     <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">{header}</div>
                 </header>
             )}
