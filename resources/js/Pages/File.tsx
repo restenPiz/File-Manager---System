79ebccb96b68f5ef40f2f5a5c46ce54d31e64b00
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-17 12:08:06 +0200

    Changing the name of the dropdowns items

--- resources/js/Pages/File.tsx
+++ resources/js/Pages/File.tsx
@@ -173,15 +173,6 @@ export default function File({ auth, folderId, files }: PageProps) {
                                                             onClick={() => { /* Função para editar */ }}
                                                             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
                                                         >
-                                                            Dowloand File
-                                                        </a>
-                                                    </Dropdown.Item>
-                                                    <Dropdown.Item>
-                                                        <a
-                                                            href="#"
-                                                            onClick={() => { /* Função para editar */ }}
-                                                            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white"
-                                                        >
                                                             Share
                                                         </a>
                                                     </Dropdown.Item>
