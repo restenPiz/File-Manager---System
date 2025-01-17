b1bd7b9a06b9b84d8dab69a33619bf124fb4ae37
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-17 11:52:34 +0200

    Make a rollback button in the file section run

--- resources/js/Pages/File.tsx
+++ resources/js/Pages/File.tsx
@@ -69,11 +69,6 @@ export default function File({ auth, folderId, files }: PageProps) {
         }
     };
 
-    const handleBack = () => {
-        // Usando a função Inertia.visit para redirecionar para a página de pastas
-        Inertia.get(route('folder.index')); // Ajuste a rota se necessário
-    };
-
     return (
         <AuthenticatedLayout
             user={auth.user}
@@ -89,7 +84,7 @@ export default function File({ auth, folderId, files }: PageProps) {
                             File Manager
                         </h2>
                         <div className="flex space-x-4">
-                            <Button className="bg-green-800 flex items-center" onClick={handleBack}>
+                            <Button className="bg-green-800 flex items-center" onClick={() => window.history.back()}>
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
                                 </svg>
