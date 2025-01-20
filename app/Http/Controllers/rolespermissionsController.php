ba5139ae140fced50a8bb7f13b4ddfcee74a6e75
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-20 10:56:41 +0200

    Some updates in the application

--- app/Http/Controllers/rolespermissionsController.php
+++ app/Http/Controllers/rolespermissionsController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permission;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class rolespermissionsController extends Controller
 {
-    //
+    public function index()
+    {
+        $permissions = Permission::all();
+
+        return Inertia::render('RolesPermissions', [
+            'permissions' => $permissions,
+        ]);
+    }
 }
