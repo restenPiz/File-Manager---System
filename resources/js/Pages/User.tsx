b938b2978cbcb9d7a46cc4dae1dfade1fe15c137
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-21 13:44:55 +0200

    Some updates in the application

--- resources/js/Pages/User.tsx
+++ resources/js/Pages/User.tsx
@@ -1,8 +1,7 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PageProps } from "@/types";
 import { Head } from "@inertiajs/react";
-import TableUser from "@/Components/TableUser";
-import { Button, Modal, Select } from "flowbite-react";
+import { Button, Modal, Select, Table } from "flowbite-react";
 import { useState } from "react";
 
 export default function User({ roles, auth }: PageProps) {
@@ -31,7 +30,32 @@ export default function User({ roles, auth }: PageProps) {
                         </Button>
                     </div>
 
-                    <TableUser></TableUser>
+                    <Table hoverable>
+                        <Table.Head>
+                            <Table.HeadCell>Username</Table.HeadCell>
+                            <Table.HeadCell>Email</Table.HeadCell>
+                            <Table.HeadCell>Role User</Table.HeadCell>
+                            <Table.HeadCell>Number Bi</Table.HeadCell>
+                            <Table.HeadCell>
+                                <span className="sr-only">Edit</span>
+                            </Table.HeadCell>
+                        </Table.Head>
+                        <Table.Body className="divide-y">
+                            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                                    {'Apple MacBook Pro 17"'}
+                                </Table.Cell>
+                                <Table.Cell>Sliver</Table.Cell>
+                                <Table.Cell>Laptop</Table.Cell>
+                                <Table.Cell>$2999</Table.Cell>
+                                <Table.Cell>
+                                    <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                                        View
+                                    </a>
+                                </Table.Cell>
+                            </Table.Row>
+                        </Table.Body>
+                    </Table>
 
                     {/* Modal */}
                     <Modal show={isModalOpen} onClose={toggleModal}>
