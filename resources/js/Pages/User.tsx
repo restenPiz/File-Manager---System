4a6f7a9068007bff3294da8932909263efa4d369
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-01-21 13:50:19 +0200

    Doing some updates in the application

--- resources/js/Pages/User.tsx
+++ resources/js/Pages/User.tsx
@@ -52,11 +52,11 @@ export default function User({ users, roles, auth }: PageProps) {
                                     users.map((user, index) => (
                                         <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
                                             <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
-                                                {'Apple MacBook Pro 17"'}
+                                                {user.name}
                                             </Table.Cell>
-                                            <Table.Cell>Sliver</Table.Cell>
-                                            <Table.Cell>Laptop</Table.Cell>
-                                            <Table.Cell>$2999</Table.Cell>
+                                            <Table.Cell>{user.email}</Table.Cell>
+                                            <Table.Cell></Table.Cell>
+                                            <Table.Cell>{user.Number_bi}</Table.Cell>
                                             <Table.Cell>
                                                 <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
                                                     Vie
