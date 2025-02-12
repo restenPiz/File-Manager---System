d74c39a989404e0b743b7504d8f2535024a8ae14
restenPiz <99766054+restenPiz@users.noreply.github.com>
2025-02-12 13:46:54 +0200

    Some updates in the app

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
 
 class dashboardController extends Controller
 {
+    public function main()
+    {
+        return Inertia::render('Auth/Login', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
     public function index()
     {
         if (auth::user()->hasRole('super')) {
